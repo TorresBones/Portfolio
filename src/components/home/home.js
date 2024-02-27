85bc00f373a01186c6399063c5c5862f61b3d640
TorresBones <jonathantorres521@gmail.com>
2024-02-26 21:06:34 -0500

    Setting up everything, adding up files and folders and completing all the components

---
+++ src/components/home/home.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+function Home() {
+    return (
+        <section>
+            <Container fluid className="home-content" id="home">
+            <Container className="home-content">
+                <Row>
+                    <Col md={7} className="home-header">
+                        <h1 style={{ paddingBottom: 15 }} className="heading">
+                            Hello!
+                        </h1>
+
+                        <h1 className="header-name">
+                            I'M Jonathan Torres
+                        </h1>
+                        <div style={{ padding: "50px", textAlign: "left" }}>
+                            <Type />
+                        </div>
+                    </Col>
+
+                    <Col md={5} style={{ paddingBottom: "20" }}>
+                    </Col>
+                </Row>
+            </Container>
+            </Container>
+        </section>
+    );
+}
+
+export default Home;
