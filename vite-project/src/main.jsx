28da24b4bce529b8d9e20e92248fdf80066c2336
TorresBones <jonathantorres521@gmail.com>
2024-03-04 19:10:01 -0500

    Debugging the whole project

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import App from './App';
+import App from "./App";
 import About from "./pages/About";
-import Home from "./pages/home";
 import Projects from "./pages/projects";
 import Resume from "./pages/resume";
 import Error from "./pages/Error";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import './index.css';
+import './styles/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Switch>
         <Route path='/About' component={About} />
-        <Route path='/home' component={Home} />
         <Route path='/projects' component={Projects} />
         <Route path='/resume' component={Resume} />
         <Route component={Error} />
--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   port: 3000,
-  open: true
+  open: true,
+  assetsInclude: ["**/*.jpg", "**/*.JPG"]
 })
