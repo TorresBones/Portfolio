8c03f1c214eeb42bd01a353f7b81d6034607d8b7
TorresBones <jonathantorres521@gmail.com>
2024-03-09 18:19:37 -0500

    Styling all the pages

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -1,10 +1,16 @@
 import resume from "../assets/Resume.pdf";
 
+import "../styles/index.css"
+
 export default function Resume() {
     return (
         <>
         <section>
-            <h2>
+            <div className="resume-container">
+            <iframe src={resume} width="100%" height="100%"></iframe>
+            </div>
+
+            <h2 id="resume-container">
                 Download my Resume{" "}
                 <a href={resume} download="Jonathan-Torres-Resume">
                     here
